6c09168a6c571cbac7290a03723d98f5c529dbb9
yigitgokcu <59539521+yigitgokcu@users.noreply.github.com>
2020-11-26 17:24:24 +0300

    Update zabbix_agent_install.sh

--- zabbix_agent_install.sh
+++ zabbix_agent_install.sh
@@ -228,7 +228,7 @@ if echo "$answer" | grep -iq "^y" ;then
     Info "PSKIdentity - $PSKIdentity$RAND_PREFIX"
 
 else
-      echo -e "Ok, your agent is will be insecure..."
+      echo -e "Ok, your agent will be insecure..."
 fi
 
 # Active agent (EnableRemoteCommands)
